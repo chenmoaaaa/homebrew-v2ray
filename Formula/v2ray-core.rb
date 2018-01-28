d927417a1d4178348813c95eb3b9c34c8114467d
Voleking <volekingsg@gmail.com>
2018-01-28 13:55:49 +0800

    Add v2ray-core.rb

---
+++ Formula/v2ray-core.rb
@@ -0,0 +1,59 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class V2rayCore < Formula
+  desc "A platform for building proxies to bypass network restrictions."
+  homepage "https://www.v2ray.com/"
+  url "https://github.com/v2ray/v2ray-core/releases/download/v3.6/v2ray-macos.zip"
+  version "3.6"
+  sha256 "0108197bb4831cd66e274f98f9c0ea5dcc3315fa4e774520874e3e4f01d89afb"
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "v2ray"
+    bin.install "v2ctl"
+    bin.install "geoip.dat"
+    bin.install "geosite.dat"
+
+    etc.install "config.json"
+  end
+
+  plist_options :manual => "v2ray -config=#{HOMEBREW_PREFIX}/etc/config.json"
+
+  def plist; <<-EOS
+  <?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+  <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{bin}/v2ray</string>
+        <string>-config</string>
+        <string>#{etc}/config.json</string>
+      </array>
+    </dict>
+  </plist>
+  EOS
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test v2ray-core`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/v2ray", "-version"
+  end
+end
